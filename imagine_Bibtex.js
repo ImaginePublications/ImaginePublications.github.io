16ae9300257229e562a7e60fa0f9720a5bac325a
Loic Landrieu <landrieu.loic@gmail.com>
2026-04-16 10:25:26 +0200

    Update imagine_Bibtex.js

--- imagine_Bibtex.js
+++ imagine_Bibtex.js
@@ -47,9 +47,9 @@ var authors_url = [
     "Lucas Ventura", "http://lucasventura.com",
     "Chaohui Wang", "https://imagine-lab.enpc.fr/staff-members/chaoi-wang/",
     "Yang Xiao", "https://youngxiao13.github.io/",
-    "Mathis Petrovich", "https://mathis.petrovich.fr/"
-	"Guillaume Astruc", "https://gastruc.github.io/"
-    "Yohann Perron", "https://yohannperron.github.io/WebPage/"
+    "Mathis Petrovich", "https://mathis.petrovich.fr/",
+	"Guillaume Astruc", "https://gastruc.github.io/",
+    "Yohann Perron", "https://yohannperron.github.io/WebPage/",
     "Louis Geist", "https://louisgeist.github.io/"
 ];
 
@@ -72,7 +72,7 @@ var expanded_venues = [
     "ICIP",  "International Conference on Image Processing", "ICIP",
     "ICLR",  "International Conference on Learning Representations", "ICLR",
     "IJCV",  "International Journal of Computer Vision", "IJCV",
-	"ICRA", "International Conference on Robotics and Automation", "ICRA"
+	"ICRA", "International Conference on Robotics and Automation", "ICRA",
     "ISMAR", "Proceedings of the International Symposium on Mixed and Augmented Reality", "ISMAR",
     "MICCAI", "Proceedings of the Conference on  Medical Image Computing and Computer Assisted Intervention", "MICCAI",
     "NIPS",  "Advances in Neural Information Processing Systems", "NIPS",
